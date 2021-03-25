927ab2779970e9eaedce025b9ee26149171275ae
natanOds <natanods4@gmail.com>
2021-03-25 20:17:35 -0300

    Login finalizado, terminando a parte List

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { useEffect } from 'react/cjs/react.development';
+
+import logo from '../assets/logo.png';;
 
 export default function List(){
-  return(
-    <View>
+  const [techs, setTechs] = useState([]);
 
-    </View>
-  );
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+
+      })
+    
+  }, []);
+
+  return(
+   <SafeAreaView style={StyleSheet.container}>
+    <Image source={logo} />
+  </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
